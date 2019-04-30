3f30a1076499fc059127ee4da713789be8f9a3ec
jflute <dbflute@gmail.com>
2019-05-01 00:52:12 +0900

    release DBFlute-1.2.0

--- src/main/java/org/docksidestage/db2/dbflute/cbean/bs/BsAliasExceptCB.java
+++ src/main/java/org/docksidestage/db2/dbflute/cbean/bs/BsAliasExceptCB.java
@@ -47,6 +47,9 @@ public class BsAliasExceptCB extends AbstractConditionBean {
             enableSpecifyColumnRequired();
         }
         xsetSpecifyColumnRequiredExceptDeterminer(DBFluteConfig.getInstance().getSpecifyColumnRequiredExceptDeterminer());
+        if (DBFluteConfig.getInstance().isSpecifyColumnRequiredWarningOnly()) {
+            xenableSpecifyColumnRequiredWarningOnly();
+        }
         if (DBFluteConfig.getInstance().isQueryUpdateCountPreCheck()) {
             enableQueryUpdateCountPreCheck();
         }
